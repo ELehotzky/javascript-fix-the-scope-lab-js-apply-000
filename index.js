bc5208cd64b24eed9891f99cfb6838602ba3214a
lehotzky.erica@gmail.com <lehotzky.erica@gmail.com>
2018-04-29 15:43:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return animal;
 }
 
 function add2(n) {
-  return n + two
+  return n + two;
 
   // Feel free to move things around!
-  const two = 2
+  const two = 2;
 }
 
 var funkyFunction = function() {
